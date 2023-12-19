d0be0aa4a97599068c88c8338c35a42b6cc6280d
FrostScythe <71485558+JaredSaenz@users.noreply.github.com>
2023-12-19 13:51:04 -0600

    Update scripts.js
    Haciendo que el menú sea deslizable

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     const sidebar = document.getElementById('sidebar');
     const content = document.getElementById('content');
+    const sidebarCollapse = document.getElementById('sidebarCollapse');
 
-    document.getElementById('sidebarCollapse').addEventListener('click', function () {
+    sidebarCollapse.addEventListener('click', function () {
         sidebar.classList.toggle('active');
         content.classList.toggle('active');
     });
